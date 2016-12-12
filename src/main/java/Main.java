9710212826ae3638ed0a46fe671ead38750ca45e
howelljoshua <joshua.d.howell@gmail.com>
2016-12-12 09:29:27 -0500

    cons msg java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -111,7 +111,8 @@ public class Main {
         
                 String firstname = req.queryParams("firstname");        
             	String lastname = req.queryParams("lastname");
-
+                System.out.print(firstname);
+                System.out.print(lastname);
                   //make new db connection, create a new hashmap to be used later for results
                 Connection connection = null;
                 Map<String, Object> attributes = new HashMap<>();  
